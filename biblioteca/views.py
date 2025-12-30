1ba899f210fed16ce72300d010e378a377a93baa
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-12-30 09:40:20 -0300

    feat(views): Criada view para listar livros

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Livro
 
-def autor(request):
-    pass
+def listar_livros(request):
+    livros = Livro.objects.all().order_by('-criado_em')
+
+    context = {
+        'livros':livros
+    }
+
+    return render(request, 'biblioteca/listar_livros.html', context)
