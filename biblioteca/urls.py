f80e268c0365ac9d43ddb6b4377ef6243bd4d30d
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-12-30 10:51:49 -0300

    feat(urls): Configura as rotas da API com o router

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+
+# Cria o router e registra as novas viewsets
+router = DefaultRouter()
+router.register(r'autores', views.AutorViewSet)
+router.register(r'livros', views.LivroViewSet)
+router.register(r'resenha', views.ResenhaViewSet)
 
 
 urlpatterns = [
     path('', views.listar_livros, name='listar_livros'),
+
+    path('api/', include(router.urls)),
 ]
